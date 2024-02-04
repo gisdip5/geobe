ca8a7d716e15ef9f2405310f90a99af5fdced1f3
Rolly Maulana Awangga <ardiva.diva30@gmail.com>
2024-02-04 19:26:33 +0700

    fiks

---
+++ main/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	dipa "github.com/Ardivadiva/geobe"
+)
+
+func HelloHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, dipa.PostGeoIntersects("MONGOSTRING", "gisjson2", "gisjson2", r))
+}
+
+func handlerRequests() {
+	http.HandleFunc("/", HelloHTTP)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+	handlerRequests()
+}
